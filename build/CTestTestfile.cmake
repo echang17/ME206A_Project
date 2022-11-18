b5d7abaa7e9cf40505d3ce9048f216d9fce96311
Class Account <ee106a-adg@c105-7.local>
2022-11-18 15:24:39 -0800

    first attempt at workspace with ar_track_alvar

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa22/class/ee106a-adg/ME206A_Project/src
+# Build directory: /home/cc/ee106a/fa22/class/ee106a-adg/ME206A_Project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ar_track_alvar/ar_track_alvar_msgs")
+subdirs("ar_track_alvar/ar_track_alvar")
